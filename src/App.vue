ad615f9ae2519b6da4ac2effe98bae856c077113
CoArrayLiu <3061632379@qq.com>
2024-12-04 21:51:46 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <gau/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import gau from './components/gaussian.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    gau
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/gaussian.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="container"></div>
+  </template>
+  
+  <script>
+  import * as GaussianSplats3D from '@mkkellogg/gaussian-splats-3d';
+  
+  
+  export default {
+    name: 'PointCloudViewer',
+    mounted() {
+      const viewer = new GaussianSplats3D.Viewer({
+        'cameraUp': [0, -1, -0.6],
+        'initialCameraPosition': [-1, -4, 6],
+        'initialCameraLookAt': [0, 4, 0]
+      });
+  
+      // Ensure the path to your model is correct (e.g., check public folder)
+      viewer.addSplatScene('model.ply', {
+        'splatAlphaRemovalThreshold': 5,
+        'showLoadingUI': true,
+        'position': [0, 1, 0],
+        'rotation': [0, 0, 0, 1],
+        'scale': [1.5, 1.5, 1.5]
+      })
+      .then(() => {
+        viewer.start();
+      }).catch((error) => {
+        console.error('Error loading the model:', error);
+      });
+    }
+  };
+  </script>
